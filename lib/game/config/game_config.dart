f90a5c7b5b4d0d58cbcf66ed77e737dc7f0e6b82
Thomas Hyaumet <Thomas.Hyaumet.Etu@univ-lemans.fr>
2024-02-08 12:01:22 +0100

    add config

---
+++ lib/game/config/game_config.dart
@@ -0,0 +1,7 @@
+class GameConfig {
+  static const rows = 45;
+
+  static const columns = 95;
+
+  static const cellSize = 20.0;
+}
---
+++ lib/game/config/styles.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class Styles {
+  static Paint land = Paint()
+    ..color = const Color(0xFFA5927D)
+    ..style = PaintingStyle.fill;
+
+  static Paint water = Paint()
+    ..color = const Color(0xFF121252)
+    ..style = PaintingStyle.fill;
+
+  static Paint mountain = Paint()
+    ..color = const Color(0xFFE6E6E6)
+    ..style = PaintingStyle.fill;
+}
